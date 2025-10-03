414584144c5b73851addd3afd81be30b4e7d0ea0
MartinLima <martinlima127@gmail.com>
2025-10-03 11:56:39 -0300

    Avance sobre los test unitarios, falta la ultima parte

---
+++ test/AmmenTravel.Application.Tests/DestinoTest/DestinoTestAppServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using AmmenTravel;
+using AmmenTravel.DestinosDTO;
+using AmmenTravel.InterfaceDestinoAppService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Modularity;
+using Xunit;
+using Shouldly;
+
+namespace AmmenTravel.DestinoTest
+{
+    public abstract class classDestinoTestAppServiceTest<TStartupModule> : AmmenTravelApplicationTestBase<TStartupModule>
+        where TStartupModule : IAbpModule
+    {
+        private readonly IDestinoAppService _service;
+
+        protected classDestinoTestAppServiceTest()
+        {
+            _service = GetRequiredService<IDestinoAppService>();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnCreateDestinoDTO()
+        {
+            var input = new CreateUpdateDestinoDTO
+            {
+                Nombre = "Paris",
+                Pais = "Francia",
+                Poblacion = 2048000,
+                FotoURL = "https://example.com/paris.jpg"
+            };
+
+            //Act
+            var result = await _service.CreateAsync(input);
+
+            //Assert
+            result.ShouldNotBeNull();
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Nombre.ShouldBe(input.Nombre);
+            result.Pais.ShouldBe(input.Pais);
+            result.Poblacion.ShouldBe(input.Poblacion);
+            result.FotoURL.ShouldBe(input.FotoURL);
+        }
+    }
+
+
+}
