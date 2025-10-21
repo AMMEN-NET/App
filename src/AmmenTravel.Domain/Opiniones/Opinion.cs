42bd3509950ab1cd601de00936d488912ffee855
aguspperez <agupperez@gmail.com>
2025-10-21 20:41:56 -0300

    Push de migracion, hicimos opinion

---
+++ src/AmmenTravel.Domain/Opiniones/Opinion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace AmmenTravel.Opiniones
+{
+    public class Opinion : FullAuditedEntity<Guid> 
+    {
+        public required ValorPuntuacion Puntuacion { get; set; }
+        public required string Comentario { get; set; }
+        public required Guid DestinoTuristicoId { get; set; } //Es para hacer la foreign key
+        public required Guid UsuarioId { get; set; } //Es para hacer la foreign key
+        
+    }
+    
+    public enum ValorPuntuacion 
+    {
+        Uno = 1,
+        Dos = 2,
+        Tres = 3,
+        Cuatro = 4,
+        Cinco = 5
+    }
+}
