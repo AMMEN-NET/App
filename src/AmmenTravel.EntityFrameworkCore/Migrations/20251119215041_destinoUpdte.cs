99bf43a5c298d76c021c5c86fbc3ad8e3d738de8
edgardo <edgardodelreal@gmail.com>
2025-11-19 19:15:54 -0300

    actualizamos destinos

---
+++ src/AmmenTravel.EntityFrameworkCore/Migrations/20251119215041_destinoUpdte.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmmenTravel.Migrations
+{
+    /// <inheritdoc />
+    public partial class destinoUpdte : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Latitud",
+                table: "AppDestinos",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Longitud",
+                table: "AppDestinos",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitud",
+                table: "AppDestinos");
+
+            migrationBuilder.DropColumn(
+                name: "Longitud",
+                table: "AppDestinos");
+        }
+    }
+}
